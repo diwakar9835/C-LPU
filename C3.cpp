8a7871e9005092b8088e606a2216af885f6fb80d
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2023-04-10 21:55:44 +0530

    Add files via upload

---
+++ C3.cpp
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+//int main(){
+//	printf("Hello world!");
+//	printf("I am learning C Programming.");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(){
+//	printf("Hello World!");
+//	printf("My name is Ankit");
+//	return 0;
+//}
+
+#include <stdio.h>
+int main(){
+	printf("My name is Ankit Diwakar.");
+	printf(" I am from Bihar,Darbhanga");
+	return 0;
+}
